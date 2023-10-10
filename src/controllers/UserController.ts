95d5aacde5aed2816209f5f6a224a51ae380be0a
certamenteoVini <viih.fv76@gmail.com>
2023-10-09 23:30:57 -0300

    declarando tipagem

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from 'express'
+import EmailService from '../services/EmailServices'
 
 const users = [{name: 'Vinícius', email: 'viih.fv76@gmail.com'}]
 
 export default {
     async index(req: Request, res: Response){
         return res.json(users)
+    },
+
+    async create(req: Request, res:Response){
+        const emailService = new EmailService()
+        
+        emailService.sendMail(
+            { name: 'Vinicius', email: 'viih.fv76@gmail.com' },
+            { subject: 'Bem-vindo ao sistema', body:'Seja bem-vindo'}
+        )
+
+        return res.send()    
+    
     }
 }
